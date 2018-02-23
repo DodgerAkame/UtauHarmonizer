4f0d2fc0a7671c786d5282e3d73af9ab1bbb281d
DodgerAkame <rmps@hotmail.fr>
2018-02-23 21:15:37 +0000

    Work on GUI

--- src/dodgerakame/view/View.java
+++ src/dodgerakame/view/View.java
@@ -1,6 +1,10 @@
 package dodgerakame.view;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 public class View {
     private JTextField pathToUSTSourceTextField;
@@ -9,6 +13,37 @@ public class View {
     private JTextField pathToUSTsToTextField;
     private JPanel MainPanel;
     private JPanel CheckboxPanel;
+
+    private JFileChooser fileChooser;
+
+    private File input;
+
+    public View() {
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    input = doSearchFile();
+                    pathToUSTSourceTextField.setToolTipText(input.getAbsolutePath());
+                }
+                catch (Exception ex){
+                    //TODO something
+                }
+            }
+        });
+    }
+
+    private File doSearchFile() throws IOException {
+        File dest = null;
+        fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        int chooserState = fileChooser.showOpenDialog(null);
+        if (chooserState == JFileChooser.APPROVE_OPTION){
+            dest = fileChooser.getSelectedFile();
+        }
+
+        return dest;
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("View");
