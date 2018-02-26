96e51d7c82ef79bf49b6191457246d91c489c382
DodgerAkame <remy.simon@uha.fr>
2018-02-26 22:38:01 +0100

    Search buttons and radio buttons added

--- src/dodgerakame/view/View.java
+++ src/dodgerakame/view/View.java
@@ -13,8 +13,15 @@ public class View {
     private JTextField pathToUSTsToTextField;
     private JPanel MainPanel;
     private JPanel CheckboxPanel;
+    private JButton GenerateBtn;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JRadioButton radioButton3;
+    private JRadioButton radioButton4;
+    private JRadioButton radioButton5;
 
     private JFileChooser fileChooser;
+    private ButtonGroup buttonGroup;
 
     private File input;
 
@@ -24,13 +31,46 @@ public class View {
             public void actionPerformed(ActionEvent e) {
                 try {
                     input = doSearchFile();
-                    pathToUSTSourceTextField.setToolTipText(input.getAbsolutePath());
+                    pathToUSTSourceTextField.setText(input.getAbsolutePath());
+                    String[] directory = input.getAbsolutePath().split(File.separator);
+                    StringBuffer output = new StringBuffer();
+
+                    for (int i = 0; i < directory.length; i++){
+                     output.append(directory[i] + File.separator);
+                    }
+
+                    output.append(directory[directory.length -1].split(".ust")[0] + " - Harmony.ust");
+                    pathToUSTsToTextField.setText(output.toString());
+
                 }
                 catch (Exception ex){
                     //TODO something
                 }
             }
         });
+
+        searchButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try{
+                    input = doSearchSaveFile();
+                    pathToUSTsToTextField.setText(input.getAbsolutePath());
+                }
+                catch (Exception e){
+
+                }
+            }
+        });
+
+
+        buttonGroup = new ButtonGroup();
+
+        buttonGroup.add(radioButton1);
+        buttonGroup.add(radioButton2);
+        buttonGroup.add(radioButton3);
+        buttonGroup.add(radioButton4);
+        buttonGroup.add(radioButton5);
+
     }
 
     private File doSearchFile() throws IOException {
@@ -39,6 +79,19 @@ public class View {
         fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         int chooserState = fileChooser.showOpenDialog(null);
         if (chooserState == JFileChooser.APPROVE_OPTION){
+            dest = fileChooser.getSelectedFile();
+        }
+
+        return dest;
+    }
+
+    private File doSearchSaveFile() throws IOException{
+        File dest = null;
+
+        fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        int chooserState = fileChooser.showSaveDialog(null);
+        if (chooserState == JFileChooser.SAVE_DIALOG){
             dest = fileChooser.getSelectedFile();
         }
 
